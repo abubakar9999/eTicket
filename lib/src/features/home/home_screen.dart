cf1d521f3deba1b2fb2665017698f080d7ef4b0b
Abu Bakar <abubakar@Abus-Mac-mini.local>
2025-09-22 23:22:18 +0600

    add home screen

--- lib/src/features/home/home_screen.dart
+++ lib/src/features/home/home_screen.dart
@@ -1,142 +1,143 @@
 
 
-// import 'package:flutter/material.dart';
+import 'package:eticket/src/features/home/widgets/home_screen_custom_button.dart';
+import 'package:flutter/material.dart';
 
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-//   @override
-//   State<HomeScreen> createState() => _HomeScreenState();
-// }
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-// class _HomeScreenState extends State<HomeScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
 
-//         appBar: AppBar(
-//           backgroundColor: AppColors.getPrimaryGradientColor(),
-//           iconTheme: const IconThemeData(
-//             color: Colors.white, // Change drawer icon color to white
-//           ),
-//           title: Text('Home Screen', style: TextStyle(color: AppColors.getBackgroundColor()),),
-//         ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.all(6.0),
-//           child: Column(
-//             children: <Widget>[
-//               const SizedBox(height: 10),
-//               Column(
-//                 children: [
+        appBar: AppBar(
+          // backgroundColor: AppColors.getPrimaryGradientColor(),
+          iconTheme: const IconThemeData(
+            color: Colors.white, // Change drawer icon color to white
+          ),
+          // title: Text('Home Screen', style: TextStyle(color: AppColors.getBackgroundColor()),),
+        ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 10),
+              Column(
+                children: [
 
-//                   Card(
-//                     elevation: 5,
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                     ),
-//                     child: Row(
-//                       children: [
+                  Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
 
-//                         Expanded(
-//                           child: CustomButton(
-//                             text: "Test",
-//                             icon: Icons.sailing_outlined,
-//                             onPressed: () {},
-//                             iconColor: Colors.white,
-//                             //backgroundColor: Colors.green, // ✅ custom background
-//                           ),
-//                         ),
+                        Expanded(
+                          child: CustomButton(
+                            text: "Test",
+                            icon: Icons.sailing_outlined,
+                            onPressed: () {},
+                            iconColor: Colors.white,
+                            //backgroundColor: Colors.green, // ✅ custom background
+                          ),
+                        ),
 
-//                       ],
-//                     ),
-//                   ),
-//                   const SizedBox(height: 5),
-//                   Card(
-//                     elevation: 5,
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                     ),
-//                     child: Row(
-//                       children: [
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
 
-//                         Expanded(
-//                           child: CustomButton(
-//                             text: "Test",
-//                             icon: Icons.account_balance_wallet,
-//                             onPressed: () {},
-//                             iconColor: Colors.white,
-//                             //backgroundColor: Colors.green, // ✅ custom background
-//                           ),
-//                         ),
-//                         const SizedBox(width: 2),
-//                         Expanded(
-//                           child: CustomButton(
-//                             text: 'Test11',
-//                             icon: Icons.adb,
-//                             iconColor: Colors.white,
-//                             onPressed: () async {
-//                             },
-//                           ),
-//                         ),
+                        Expanded(
+                          child: CustomButton(
+                            text: "Test",
+                            icon: Icons.account_balance_wallet,
+                            onPressed: () {},
+                            iconColor: Colors.white,
+                            //backgroundColor: Colors.green, // ✅ custom background
+                          ),
+                        ),
+                        const SizedBox(width: 2),
+                        Expanded(
+                          child: CustomButton(
+                            text: 'Test11',
+                            icon: Icons.adb,
+                            iconColor: Colors.white,
+                            onPressed: () async {
+                            },
+                          ),
+                        ),
 
-//                         const SizedBox(width: 2),
-//                         Expanded(
-//                           child: CustomButton(
-//                             text: 'Test11',
-//                             icon: Icons.add_alarm_sharp,
-//                             iconColor: Colors.white,
-//                             onPressed: () async {
-//                             },
-//                           ),
-//                         )
+                        const SizedBox(width: 2),
+                        Expanded(
+                          child: CustomButton(
+                            text: 'Test11',
+                            icon: Icons.add_alarm_sharp,
+                            iconColor: Colors.white,
+                            onPressed: () async {
+                            },
+                          ),
+                        )
 
-//                       ],
-//                     ),
-//                   ),
-//                   const SizedBox(height: 5),
-//                   Card(
-//                     elevation: 5,
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(10),
-//                     ),
-//                     child: Row(
-//                       children: [
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
 
-//                         Expanded(
-//                           child: CustomButton(
-//                             text: "Test",
-//                             icon: Icons.tips_and_updates,
-//                             onPressed: () {},
-//                             iconColor: Colors.white,
-//                             //backgroundColor: Colors.green, // ✅ custom background
-//                           ),
-//                         ),
-//                         const SizedBox(width: 2),
-//                         Expanded(
-//                           child: CustomButton(
-//                             text: 'Test11',
-//                             icon: Icons.backup_table,
-//                             iconColor: Colors.white,
-//                             onPressed: () async {
-//                             },
-//                           ),
-//                         )
+                        Expanded(
+                          child: CustomButton(
+                            text: "Test",
+                            icon: Icons.tips_and_updates,
+                            onPressed: () {},
+                            iconColor: Colors.white,
+                            //backgroundColor: Colors.green, // ✅ custom background
+                          ),
+                        ),
+                        const SizedBox(width: 2),
+                        Expanded(
+                          child: CustomButton(
+                            text: 'Test11',
+                            icon: Icons.backup_table,
+                            iconColor: Colors.white,
+                            onPressed: () async {
+                            },
+                          ),
+                        )
 
-//                       ],
-//                     ),
-//                   ),
+                      ],
+                    ),
+                  ),
 
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 
 
--- lib/src/features/splash/splash_screen.dart
+++ lib/src/features/splash/splash_screen.dart
@@ -1,32 +1,39 @@
-// import 'package:eticket/config/routes/routes_name.dart';
-// import 'package:eticket/services/splash/splash_services.dart';
-// import 'package:flutter/material.dart';
 
-// class SplashScreen extends StatefulWidget {
-//   const SplashScreen({super.key});
+import 'package:eticket/src/core/utils/context.dart';
+import 'package:eticket/src/features/home/home_screen.dart';
+import 'package:flutter/material.dart';
 
-//   @override
-//   State<SplashScreen> createState() => _SplashScreenState();
-// }
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
-// class _SplashScreenState extends State<SplashScreen> {
-//   SplashServices _splashServices = SplashServices();
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     _splashServices.isLogin(context);
-//   }
+class _SplashScreenState extends State<SplashScreen> {
+ 
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//           child: Text(
-//         "Splash Screen ",
-//         style: TextStyle(fontSize: 50),
-//       )),
-//     );
-//   }
-// }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      context.fadePushRemoveUntil(  const HomeScreen());
+      // Navigator.of(context).pushReplacement(
+      //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+      // );
+    });
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+        "Splash Screen ",
+        style: TextStyle(fontSize: 50),
+      )),
+    );
+  }
+}
